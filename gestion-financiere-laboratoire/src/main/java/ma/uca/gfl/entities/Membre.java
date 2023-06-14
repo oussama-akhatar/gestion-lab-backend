00db8e97a041f737f2e0ad0a60737f2162ea7ba0
akhatar <o.akhatar5927@gmail.com>
2023-06-14 10:51:51 +0100

    Some general edits

--- gestion-financiere-laboratoire/src/main/java/ma/uca/gfl/entities/Membre.java
+++ gestion-financiere-laboratoire/src/main/java/ma/uca/gfl/entities/Membre.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -24,20 +25,19 @@ public class Membre {
 	private boolean directeur;
 
 	@ManyToOne
-//	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
 	@JsonIgnoreProperties("membres")
 	private Laboratoire laboratoire;
 
 	@OneToMany(mappedBy = "membre", cascade = CascadeType.ALL)
-	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@JsonIgnoreProperties(value = "membre")
 	private List<MembreDotationUCARech> membreDotationUCARechs;
 
 	@OneToMany(mappedBy = "membre", cascade = CascadeType.ALL)
-	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@JsonIgnoreProperties(value = "membre")
 	private List<ProjetMembre> projetMembres;
 
 	@OneToMany(mappedBy = "membre", cascade = CascadeType.ALL)
-	@JsonIgnoreProperties("membre")
+	@JsonIgnoreProperties(value = "membre")
 	private List<ExpressionBesoin> expressionBesoins;
 
 }
